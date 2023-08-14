40bfa19b56a85cf29557f3c3b0360b58ca4af8cb
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2023-08-14 17:20:10 -0300

    fazendo o exemplo 3

---
+++ cap2/js/ex2_3.js
@@ -0,0 +1,27 @@
+//criando referência ao form e aos elementos de resposta (pelo seu id) 
+const frm = document.querySelector("frm")
+const resp1 = document.querySelector("outResp1")
+const resp2 = documet.querySelector("outResp2")
+const resp3 = document.querySelector("outResp3")
+const resp4 = document.querySelector("outResp4")
+
+//criando um "ouvinte" de evento, acionado quando o botão submit for clicado
+
+frm.addEventListener(
+    "submit", (e) =>{
+        const marca = frm.inVeiculo.value // pegando o valor do campo do nome marca do veículo
+        const mod = frm.inModelo.value // pegando o valor do campo do nome modelo do veículo
+        const preco = Number(frm.inValor.value) //  pegando o valor do campo do preço do veículo
+
+        const entrada = preco * 0.50 // calculando o valor de 50% da entrada 
+        const parcelado = preco/ 12 //  calculando o valor parcelado
+
+        resp1.innerText = `Promoção do carro da marca: ${marca}`
+        resp2.innerText = `O medolo do carro é: ${mod}`
+        resp3.innerText = `O valor de 50% do carro é:  ${entrada.toFixed(2)}`
+        resp4.innerText = `O valor das parcela em 12x é : ${parcelado.toFixed(2)}`
+
+        e.preventDefault() // envio do fomr
+
+    }
+)
