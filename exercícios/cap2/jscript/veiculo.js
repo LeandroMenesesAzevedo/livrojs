cffc1aadbdefb625307e8ca3c388ec567b883fb0
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2024-09-17 20:11:50 -0300

    Finalizado o exercício cap 2 b

--- exercícios/cap2/jscript/veiculo.js
+++ exercícios/cap2/jscript/veiculo.js
@@ -1,13 +1,22 @@
-// Criando referência 
+// Criando referência do form e ao elementos de resposta
 
 const frm = document.querySelector("form")
-const veiculo = document.querySelector("h4")
-const valor = document.querySelector("h3")
+const resposta1 = document.querySelector("h3")
+const resposta2 = document.querySelector("h4")
 
-frm.addEventListener( "submit", e =>{
+//Criando um ouvinte  de evento, ao clicar no submit
+frm.addEventListener( "submit", e => {
 
-    
-}
+    const veiculo = frm.imodelo.value
+    const valor = Number(frm.ivalor.value)
+
+    const valenter = valor * 0.50
+    const parcelado = valenter/12
+
+    resposta1.innerText = ` O modelo do veículo é: ${veiculo}`
+    resposta2.innerText = ` O valor do carro: ${valor.toFixed(2)}\n  Entrada de 50%: ${valenter.toFixed(2)}\n  Parcelas de 12x ${parcelado.toFixed(2)}`
 
 
-)
+
+    e.preventDefault()
+});
