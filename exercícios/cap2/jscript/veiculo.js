e1a2c93cc4cfeedd7b7d6510b2f08092173dacc9
Leandro Meneses Azevedo <110135554+LeandroMenesesAzevedo@users.noreply.github.com>
2024-09-17 19:57:47 -0300

    Fazendo o exercício cap2 b

---
+++ exercícios/cap2/jscript/veiculo.js
@@ -0,0 +1,13 @@
+// Criando referência 
+
+const frm = document.querySelector("form")
+const veiculo = document.querySelector("h4")
+const valor = document.querySelector("h3")
+
+frm.addEventListener( "submit", e =>{
+
+    
+}
+
+
+)
